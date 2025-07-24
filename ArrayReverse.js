64f4a740e37f5db3f43d636369df2440f0becd0e
Amir-Abdullah013 <amirabdullah2508@gmail.com>
2025-07-24 08:48:43 +0500

    Use map

--- ArrayReverse.js
+++ ArrayReverse.js
@@ -1,6 +1,6 @@
 let array = [1, 2, 10, 15, 30];
 let arrayReverse = [];
-array.forEach((element, index) => {
-  arrayReverse[index] = array[array.length - (1 + index)];
-});
+arrayReverse = array.map((element,index,array)=>{
+return array[array.length- (index+1)];
+})
 console.log(arrayReverse);
